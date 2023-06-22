467a7c65524e39e6631aa954b3dab1db012ad9bb
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-22 16:18:22 +0300

    betty styling + insertion_sort implementation

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+
+/**
+ * insertion_sort_list - Prints an array of integers
+ *
+ * @array: The linkedlist to be sorted
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head = *list;
+	listint_t *node = *list;
+	listint_t *temp;
+
+	while (node != NULL)
+	{
+		temp = node->next;
+		while (node->prev != NULL)
+		{
+			if (node->prev->n > node->n)
+			{
+				node->prev->next = node->next;
+				if (node->next != NULL)
+					node->next->prev = node->prev;
+
+				node->next = node->prev;
+				node->prev = node->next->prev;
+
+				if (node->prev != NULL)
+					node->prev->next = node;
+				node->next->prev = node;
+
+				if (node->prev == NULL)
+					head = node;
+
+				print_list(head);
+			}
+			else
+				break;
+		}
+		node = temp;
+	}
+	*list = head;
+}
